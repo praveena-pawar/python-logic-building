8ba34980cacebe0b43ddbaf75d3d4f6d7255ba42
praveena-pawar <praveena.dev02@gmail.com>
2025-11-20 11:39:15 +0530

    Take two numbers from the user and print the bigger number

--- logic.py
+++ logic.py
@@ -1,1 +1,128 @@
-print("hello world")
+# print("hello world")
+
+
+# Q1 Take two numbers from the user and print the bigger number
+
+num1 = int(input("Enter fisr number :"))
+num2 = int(input("Enter second number :"))
+if num1 > num2 :
+    print("bigger number is :",num1)
+else :
+    print("bigger number is :",num2)
+
+
+
+
+
+
+
+
+
+# @2 Take a number from the user and print whether it is EVEN or ODD.
+
+# n = int(input("Enter number :"))
+# if (n % 2 == 0):
+#     print("number is even")
+# else :
+#     print("number is odd")
+
+
+
+
+
+
+# @3 Take age from user and check:
+# 18 or more → “Eligible to vote”
+# Less than 18 → “Not eligible”
+
+# age = int(input("Enter age :"))
+# if age >= 18 :
+#     print("Eligible for vote..")
+# else:
+#     print("not eligible..")
+
+
+
+
+
+
+
+
+#@4 Take three numbers from the user and print the largest number.
+
+# num1 = int(input("Enter first number :"))
+# num2 = int(input("Enter second number :"))
+# num3 = int(input("Enter third number :"))
+# if (num1 > num2) and (num1 > num3):
+#     print("the largest number is :",num1)
+# elif (num2 > num1) and (num2 > num3):
+#     print("the largest number is :",num2)
+# else :
+#     print("the largest number is :",num3)
+
+
+
+
+
+
+
+#@5 Take a number from user and check:
+
+# If divisible by 3 and 5 → print “FizzBuzz”
+
+# If divisible by 3 only → print “Fizz”
+
+# If divisible by 5 only → print “Buzz”
+
+# Otherwise print the number
+
+# n = int(input("Enter number :"))
+# if (n%3 == 0) and (n%5 == 0) :
+#     print("FizzBuzz")
+# elif (n % 3 == 0):
+#     print("Fizz")
+# elif(n % 5 == 0):
+#     print("Buzz")
+# else:
+#     print(n)
+
+
+
+
+
+#Q5 Take a year from the user and check whether it is a leap year or not.
+# year = int(input("Enter a year :"))
+# if (year % 400 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print("leap year.")
+# else:
+#     print("not leap year")
+
+
+
+
+
+
+
+#Q7 Take a number from the user and check whether it is a 3-digit, 4-digit, or 5-digit number.
+# Print the correct message
+
+# n = (input("Enter number :"))
+# digits = len(str(n))
+# if digits == 3:
+#     print("3-digit number")
+# elif digits == 4:
+#     print("4-digit number")
+# elif digits == 5:
+#     print("5-digit number")
+# else:
+#     print("Not 3, 4, or 5 digits")
+
+
+
+
+
+
+
+
+
+# n = int(input("enter number"))
